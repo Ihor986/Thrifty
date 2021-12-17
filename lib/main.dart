61280d353420420b3d2b746859099391c5def022
Ihor986 <235689ks@gmail.com>
2021-12-18 01:34:11 +0200

    cosm

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:hive/hive.dart';
+// import 'package:hive/hive.dart';
 import 'package:thrifty/vars/text.dart';
 import 'bloc/my_home_page_bloc/home_page_bloc.dart';
 import 'pages/home_page/home_page.dart';
--- lib/pages/settings_drower/settings_drower_counts.dart
+++ lib/pages/settings_drower/settings_drower_counts.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_locales/flutter_locales.dart';
 import 'package:thrifty/bloc/my_home_page_bloc/home_page_bloc.dart';
